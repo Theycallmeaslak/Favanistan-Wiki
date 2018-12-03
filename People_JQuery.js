dbcf373016907880fb1a70f934e593c7c1953ddb
riphiphip <halvor.bjorstad@gmail.com>
2018-12-03 21:29:05 +0100

    jQuery for personbokser: Åpne/Lukke

---
+++ People_JQuery.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  /*Åpner og lukker personbokser*/
+  $(".personbox").click(function() {
+    if ($(this).css("--expanded") == "1") {
+      console.log("hello world!")
+      $(this).css({
+        "height": "",
+        "--expanded": "",
+        "opacity": ""
+      });
+      $(".unimportant").css
+    } else {
+      console.log("hello")
+      $(this).css({
+        "height": "auto",
+        "opacity": "1",
+        "--expanded": "1"
+      });
+    }
+  });
+
+});
